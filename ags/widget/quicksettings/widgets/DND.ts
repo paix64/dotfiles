95f32ec1db2d95007347339d1040f6a06ca0c132
paixeww <paixeww@gmail.com>
2024-07-20 20:22:36 +0300

    removed powermenu

--- ags/widget/quicksettings/widgets/DND.ts
+++ ags/widget/quicksettings/widgets/DND.ts
@@ -5,8 +5,8 @@ const n = await Service.import("notifications")
 const dnd = n.bind("dnd")
 
 export const DND = () => SimpleToggleButton({
-    icon: dnd.as(dnd => icons.color[dnd ? "dark" : "light"]),
-    label: dnd.as(dnd => dnd ? "Night" : "Day"),
+    icon: dnd.as(dnd => icons.color[dnd ? "light" : "light"]),
+    label: dnd.as(dnd => dnd ? "Night Light" : "Night Light"),
     toggle: () => n.dnd = !n.dnd,
     connection: [n, () => n.dnd],
 })
