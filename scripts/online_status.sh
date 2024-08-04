a0056294c97423bc01253cc8816d8ad69f8dcb03
paixeww <paixeww@gmail.com>
2024-08-04 19:32:49 +0300

    scripts

--- scripts/online_status.sh
+++ scripts/online_status.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 if ping -c 1 google.com 2>/dev/null | grep -q '1 received'; then
-	mkdir -p log
-    echo " Online" > ./log/status.log
+    echo " Online" 
     exit 0
 else
-	mkdir -p log
-    echo " Offline" > ./log/status.log
+    echo " Offline"
     exit 1
 fi
