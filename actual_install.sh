d80af557ad15bf55f432c6d6d716e598aecaf7d3
paiyayay <paixeww@gmail.com>
2024-08-06 23:17:23 +0300

    config

--- actual_install.sh
+++ actual_install.sh
@@ -2,6 +2,8 @@
 
 install_yay() {
     echo ":: Installing yay..."
+    sleep .4
+    
     sudo pacman -Syu --noconfirm
     sudo pacman -S --needed --noconfirm base-devel git
     git clone https://aur.archlinux.org/yay.git /tmp/yay
@@ -9,20 +11,121 @@ install_yay() {
     makepkg -si --noconfirm --needed
 }
 
-rate_mirrors() {
-rate-mirrors --save /tmp/mirrorlist arch
-sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sudo mv /tmp/mirrorlist /etc/pacman.d/mirrorlist 
+install_packages() {
+	echo ":: Installing packages"
+    sleep .4
+    
+    yay -Syu --noconfirm --needed
+    yay -S --needed - < packages.conf
 }
 
-ln -s $HOME/dotfiles/ags $HOME/.config/ags
-ln -s $HOME/dotfiles/kitty $HOME/.config/kitty
-ln -s $HOME/dotfiles/hypr $HOME/.config/hypr
-ln -s $HOME/dotfiles/fish $HOME/.config/fish
-ln -s $HOME/dotfiles/fastfetch $HOME/.config/fastfetch
-ln -s $HOME/dotfiles/oh-my-posh $HOME/.config/oh-my-posh
-ln -s $HOME/dotfiles/localshare/zoxide $HOME/.local/share/
-ln -s $HOME/dotfiles/rofi $HOME/.config/
-ln -s $HOME/dotfiles/nvim $HOME/.config/
+setup_sensors() {
+	echo ":: Setting up sensors"
+	sleep .4
+	
+    sudo sensors-detect --auto >/dev/null
+}
+
+check_config_folders() {
+	echo ":: Checking for existing folders"
+	sleep .4
+	
+    local CHECK_CONFIG_FOLDERS="ags kitty hypr fastfetch fish oh-my-posh rofi nvim"
+    local DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
+    local EXISTING="NO"
+
+    mkdir -p "$HOME/.backup/$DATETIME/"
+
+    for dir in $CHECK_CONFIG_FOLDERS; do
+        if [ -d "$HOME/.config/$dir" ]; then
+            echo ":: Attention: directory $dir already exists in .config"
+            mv $HOME/.config/$dir "$HOME/.backup/$DATETIME/"
+            EXISTING="YES"
+        fi
+    done
+
+    if [[ $EXISTING == "YES" ]]; then
+        echo ":: Old config folder(s) backed up at ~/.backup folder"
+    fi
+}
+
+rate_mirrors() {
+	echo ":: Rating mirrors"
+	sleep .4
+	
+	rate-mirrors --save /tmp/mirrorlist arch
+	sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+	sudo mv /tmp/mirrorlist /etc/pacman.d/mirrorlist 
+}
+
+copy_config_folders() {
+	check_config_folders
+
+	echo ":: Creating links"
+	sleep .4
+	
+	ln -s $HOME/dotfiles/ags $HOME/.config/ags
+	ln -s $HOME/dotfiles/kitty $HOME/.config/kitty
+	ln -s $HOME/dotfiles/hypr $HOME/.config/hypr
+	ln -s $HOME/dotfiles/fish $HOME/.config/fish
+	ln -s $HOME/dotfiles/fastfetch $HOME/.config/fastfetch
+	ln -s $HOME/dotfiles/oh-my-posh $HOME/.config/oh-my-posh
+	ln -s $HOME/dotfiles/localshare/zoxide $HOME/.local/share/
+	ln -s $HOME/dotfiles/rofi $HOME/.config/
+	ln -s $HOME/dotfiles/nvim $HOME/.config/
+}
+
+setup_services() {
+    echo ":: Setting up services"
+    sleep .4
+
+    if systemctl is-active --quiet bluetooth.service; then
+        echo ":: bluetooth.service already running."
+    else
+        sudo systemctl enable bluetooth.service
+        sudo systemctl start bluetooth.service
+        echo ":: bluetooth.service activated successfully."
+    fi
+
+    if systemctl is-active --quiet NetworkManager.service; then
+        echo ":: NetworkManager.service already running."
+    else
+        sudo systemctl enable NetworkManager.service
+        sudo systemctl start NetworkManager.service
+        echo ":: NetworkManager.service activated successfully."
+    fi
+}
+
+update_user_dirs() {
+    echo ":: Creating user directories"
+    sleep .4
+    
+    xdg-user-dirs-update
+}
+
+install_theme() {
+	echo ":: Installing Themes"
+	sleep .4
+
+	color_scheme="prefer-dark"
+	gtk_theme="adw-gtk3-dark"
+	icon_theme="Papirus-Dark"
+	cursor_theme="Bibata-Modern-Ice"
+
+	gsettings set org.gnome.desktop.interface color-scheme $color_scheme > /dev/null 2>&1 &
+	gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme > /dev/null 2>&1 &
+	gsettings set org.gnome.desktop.interface icon-theme $icon_theme > /dev/null 2>&1 &
+	gsettings set org.gnome.desktop.interface cursor-theme $cursor_theme > /dev/null 2>&1 &
+	gsettings set org.gnome.desktop.interface cursor-size 24 > /dev/null 2>&1 & 
+	gsettings set org.gnome.desktop.interface font-name "Ubuntu Nerd Font Bold 12" > /dev/null 2>&1 &
+}
+
+finalize() {
+    echo ":: Finishing installation"
+    sleep .4
+    
+    hyprctl reload
+    ags --init
+}
 
 install_yay
