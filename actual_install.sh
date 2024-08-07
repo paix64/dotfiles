ceb809c9e3fdbfda688120df8481c34cb9887083
paiyayay <paixeww@gmail.com>
2024-08-07 12:03:14 +0300

    nvidia hook

--- actual_install.sh
+++ actual_install.sh
@@ -215,6 +215,23 @@ setup_nvidia() {
 	sudo cp /etc/default/grub /etc/default/grub.bak
 	sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT="quiet loglevel=3 nvidia_drm.modeset=1"/' /etc/default/grub
 	sudo grub-mkconfig -o /boot/grub/grub.cfg
+	
+	sudo mkdir -p /etc/pacman.d/hooks/
+	echo "[Trigger]
+	Operation=Install
+	Operation=Upgrade
+	Operation=Remove
+	Type=Package
+	Target=nvidia
+	Target=linux
+
+	[Action]
+	Description=Updating NVIDIA module in initcpio
+	Depends=mkinitcpio
+	When=PostTransaction
+	NeedsTargets
+	Exec=/bin/sh -c 'while read -r trg; do case $trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+	" | sudo tee /etc/pacman.d/hooks/nvidia.hook
 }
 
 
