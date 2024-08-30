0a5972c172165fe68c81a334320ad6f7dca77eb1
paiyayay <paixeww@gmail.com>
2024-08-30 13:10:20 +0300

    wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,38 +1,37 @@
 local wezterm = require("wezterm")
-local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+local config = {
+	initial_cols = 80,
+	initial_rows = 20,
 
-config.initial_cols = 80
-config.initial_rows = 20
+	window_padding = {
+		top = 10,
+		left = 10,
+		right = 10,
+		bottom = 0,
+	},
 
-config.font_size = 16.0
-config.font = wezterm.font("Jetbrains Mono Nerd Font", { weight = "Bold" })
-config.color_scheme = "Gruvbox dark, hard (base16)"
-config.hide_tab_bar_if_only_one_tab = true
-config.selection_word_boundary = "\t\n{}[]()\"'`-/.,;:❯ "
+	enable_tab_bar = false,
 
-config.colors = {
-	-- foreground = "#00FF00",
-	-- background = "black",
+	window_background_opacity = 0.60,
 
-	-- cursor_bg = "#FFFFFF",
-	-- cursor_fg = "black",
+	font_size = 16.0,
 
-	-- selection_fg = "black",
-	--selection_bg = "#00ff00",
+	font = wezterm.font("Jetbrains Mono Nerd Font", { weight = "Bold" }),
+	color_scheme = "Gruvbox dark, hard (base16)",
+	selection_word_boundary = "\t\n{}[]()\"'`-/.,;:❯ ",
+	line_height = 0.90,
+	colors = {
+		-- foreground = "#00FF00",
+		-- background = "black",
 
-	--split = "#444444",
+		-- cursor_bg = "#FFFFFF",
+		-- cursor_fg = "black",
+
+		-- selection_fg = "black",
+		--selection_bg = "#00ff00",
+
+		--split = "#444444",
+	},
 }
-
-config.window_frame = {
-	active_titlebar_bg = "rgba(0 0 0 0)",
-}
-
-config.window_background_opacity = 0.60
-
 return config
