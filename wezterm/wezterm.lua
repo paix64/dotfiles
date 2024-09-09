d66dc46ed260e3d9cf93652c4760d5ebab29168e
paiyayay <paixeww@gmail.com>
2024-09-09 13:05:37 +0300

    wezterm conf

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local act = wezterm.action
 local bar = require("bar")
 
 local config = {
-	initial_cols = 80,
-	initial_rows = 20,
+	initial_cols = 64,
+	initial_rows = 22,
 
 	window_padding = {
 		top = 10,
