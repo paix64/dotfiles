92268027d1f22c0cff64e20f4795e36535376ee4
paiyayay <paixeww@gmail.com>
2024-09-29 16:30:29 +0300

    wezterm conf

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -39,15 +39,44 @@ local config = {
 		--split = "#444444",
 	},
 	keys = {
-		{ key = "F1", mods = "", action = act.SpawnTab("CurrentPaneDomain") },
-		{ key = "w", mods = "CTRL", action = act.CloseCurrentTab({ confirm = true }) },
-		{ key = "1", mods = "ALT", action = act.ActivateTab(0) },
-		{ key = "2", mods = "ALT", action = act.ActivateTab(1) },
-		{ key = "3", mods = "ALT", action = act.ActivateTab(2) },
-		{ key = "4", mods = "ALT", action = act.ActivateTab(3) },
-		{ key = "5", mods = "ALT", action = act.ActivateTab(4) },
+		{ key = "F1", mods = "",     action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "w",  mods = "CTRL", action = act.CloseCurrentTab({ confirm = true }) },
+		{ key = "1",  mods = "ALT",  action = act.ActivateTab(0) },
+		{ key = "2",  mods = "ALT",  action = act.ActivateTab(1) },
+		{ key = "3",  mods = "ALT",  action = act.ActivateTab(2) },
+		{ key = "4",  mods = "ALT",  action = act.ActivateTab(3) },
+		{ key = "5",  mods = "ALT",  action = act.ActivateTab(4) },
+
+		{ key = "p",  mods = "CTRL", action = act.EmitEvent("padding-off") },
+		{ key = "o",  mods = "CTRL", action = act.EmitEvent("toggle-opacity") },
 	},
 }
+
+wezterm.on("padding-off", function(window)
+	local overrides = window:get_config_overrides() or {}
+	if not overrides.window_padding then
+		overrides.window_padding = {
+			top = "0",
+			right = "0",
+			bottom = "0",
+			left = "0",
+		}
+	else
+		overrides.window_padding = nil
+	end
+	window:set_config_overrides(overrides)
+end)
+
+wezterm.on("toggle-opacity", function(window)
+	local overrides = window:get_config_overrides() or {}
+	if not overrides.window_background_opacity then
+		overrides.window_background_opacity = 1
+	else
+		overrides.window_background_opacity = nil
+	end
+	window:set_config_overrides(overrides)
+end)
+
 bar.apply_to_config(config, {
 	position = "top",
 	enabled_modules = {
@@ -59,4 +88,5 @@ bar.apply_to_config(config, {
 		hostname = false,
 	},
 })
+
 return config
