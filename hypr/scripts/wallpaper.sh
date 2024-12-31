ca5de497055e8c342c9c1484dd39118e9705155f
paiyayay <paixeww@gmail.com>
2024-12-31 14:03:37 +0200

    pywal for ghostty hyprland and wlogout

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -9,12 +9,12 @@ wallpaper_path=$HOME/Pictures/Wallpapers
 if [[ $1 == "-r" ]]; then
     wallpapers=($(find "$wallpaper_path" -type f))
     random_wallpaper=${wallpapers[RANDOM % ${#wallpapers[@]}]}
-    swww img "$random_wallpaper"
+    swww img $random_wallpaper && wal --contrast 4.5 -i $random_wallpaper 
 else
     selection="$(ls $wallpaper_path | rofi -dmenu || pkill rofi)"
     while [[ ! -f $wallpaper_path/$selection ]]; do
         wallpaper_path="$wallpaper_path/$selection"
         selection="$(ls $wallpaper_path | rofi -dmenu || pkill rofi)"
     done
-    swww img $wallpaper_path/$selection
+    swww img $wallpaper_path/$selection && wal --contrast 4.5 -i $wallpaper_path/$selection
 fi
