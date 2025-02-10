de5b0917e8f930a47503979ba99dc588e4a517a7
paix64 <paixeww@gmail.com>
2025-02-10 13:46:55 +0300

    stuff

--- scripts/battery_info.sh
+++ scripts/battery_info.sh
@@ -1,27 +1,23 @@
-#!/bin/bash
+#!/bin/env bash
 
 # Check if acpi command is available
 if ! command -v acpi &> /dev/null; then
-    echo "Error: acpi command not found. Install acpi package (e.g., sudo apt install acpi) and try again."
+    echo "Error: acpi command not found. Install acpi package and try again."
     exit 1
 fi
 
-# Get battery information
-battery_info=$(acpi -b)
-
-# Check if battery information is available
-if [[ -z "$battery_info" ]]; then
-    echo "Error: Failed to retrieve battery information."
+if ! command -v bc &> /dev/null; then
+    echo "Error: bc command not found. Install bc package and try again."
     exit 1
 fi
 
 # Extract remaining capacity, status, and time remaining from acpi output
-remaining_capacity=$(echo "$battery_info" | awk '{print $4}' | tr -d '%,')
-remaining_capacity_plugged=$(echo $battery_info | grep -o '[0-9]\+%'| head -n 1)
-status=$(echo "$battery_info" | awk '{print $3}' | tr -d ',')
-time_remaining=$(echo "$battery_info" | awk '{print $5}')
+remaining_capacity=$(acpi -b | head -n 1 | awk '{print $4}' | tr -d '%,')
+remaining_capacity_plugged=$(acpi -b | head -n 1 | grep -o '[0-9]\+%'| head -n 1)
+status=$(acpi -b | head -n 1 | awk '{print $3}' | tr -d ',')
+time_remaining=$(acpi -b | head -n 1 | awk '{print $5}')
 current_power=$(echo "scale=2; $(cat /sys/class/power_supply/BAT0/power_now) / 1000000" | bc)
-battery_health=$(echo $(acpi -i) | grep 'last full capacity' | awk -F'=' '{print $2}')
+battery_health=$(acpi -i | head -n 2 | grep 'last full capacity' | awk -F'=' '{print $2}')
 
 # Check if the battery is discharging
 if [[ "$status" != "Discharging" ]]; then
